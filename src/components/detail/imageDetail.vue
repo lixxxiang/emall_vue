8205b488b0209428be3029be66a1bebb25ee7924
lixxiang <1548806494@qq.com>
2018-05-31 17:11:24 +0800

    """""

---
+++ src/components/detail/imageDetail.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="title">
+      <div class="tab">
+        <a class="tab-item1">每日一图</a>
+        <a class="tab-item2">发生位置</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Tab, TabItem } from 'vux'
+export default {
+  components: {
+    Tab,
+    TabItem
+  },
+  name: 'imageDetail'
+}
+</script>
+
+<style scoped lang="less" rel="stylesheet/less">
+  .title{
+    background-color: #6299FF;
+    width: 100%;
+    height: 189px;
+    position: absolute;
+  }
+  .tab{
+    width: 169px;
+    height: 22px;
+    margin:31px auto auto auto;
+    background: #f2056e;
+  }
+  .tab-item1{
+    margin: ;
+  }
+</style>
--- src/components/homelist/Homelist.vue
+++ src/components/homelist/Homelist.vue
@@ -2,21 +2,42 @@
   <div>
     <ul class="ulclass">
       <li v-for="(a,i) in message" :key="i" class="liclass">
-        <div class="style1_div" v-if="a.style == 1">
+        <div class="style1_div" v-if="a.style == 1 && a.type == 1" @click="toImageDetail(a.contentId)">
+          <div class="style1_img_div">
+            <img :src="a.thumbnail1Path" class="style1_img">
+          </div>
           <div class="style1_title">
             {{a.contentName}}
           </div>
-          <img :src="a.thumbnail1Path" class="style1_img">
+          <div class="style1_date_div">
+            每日一图 · {{a.contentDate.slice(5)}}
+          </div>
         </div>
-        <div class="style2_div" v-if="a.style == 2">
-          <div>
+
+        <div class="style2_div" v-if="a.style == 2 && a.type == 1" @click="toImageDetail(a.contentId)">
+          <div class="style2_title">
             {{a.contentName}}
           </div>
-          <div class="imgdiv">
-            <img :src="a.thumbnail1Path" class="imgclass1">
-            <img :src="a.thumbnail2Path" class="imgclass1">
-            <img :src="a.thumbnail3Path" class="imgclass1">
-            <!--<img v-if="a.thumbnail3Path.length != 0" :src="a.thumbnail3Path" class="imgclass1">-->
+          <div class="style2_img_div">
+            <img :src="a.thumbnail1Path" class="style2_img">
+            <img :src="a.thumbnail2Path" class="style2_img">
+            <img :src="a.thumbnail3Path" class="style2_img">
+            <!--<img v-show="a.thumbnail3Path.length != 0" :src="a.thumbnail3Path" class="style2_img">-->
+          </div>
+          <div class="style2_date_div">
+            每日一图 · {{a.contentDate.slice(5)}}
+          </div>
+        </div>
+
+        <div class="type2_div" v-if="a.type == 2">
+          <div class="type2_title">
+            {{a.contentName}}
+          </div>
+          <div class="video_img_div">
+            <img :src="a.thumbnail1Path" class="video_img">
+          </div>
+          <div class="type2_date_div">
+            脉动地球 · {{a.contentDate.slice(5)}}
           </div>
         </div>
       </li>
@@ -26,7 +47,6 @@
 
 <script>
 import axios from 'axios'
-
 var message = {}
 export default {
   name: 'home_list',
@@ -51,6 +71,12 @@ export default {
           console.log(response)
         })
     })
+  },
+  methods: {
+    toImageDetail (contentId) {
+      console.log(contentId)
+      this.$router.push('../detail/imageDetail')
+    }
   }
 }
 </script>
@@ -67,42 +93,88 @@ export default {
     margin: 0px;
   }
 
-  .style2_div{
+  .style2_div {
     margin: 14px 18px;
+    height: 211px;
+    border-bottom: solid 1px #ECECEC;
+
   }
-  .style1_div{
+
+  .style1_div {
     margin: 14px 18px;
-    vertical-align: middle;
+    height: 138px;
+    border-bottom: solid 1px #ECECEC;
+
   }
+
   .imgclass {
     width: 100px;
     height: 100px;
   }
-  .imgclass1{
+
+  .style2_img {
     margin-top: 14px;
     height: 110px;
     float: left;
-    width: 31.3%;
-    margin-right: 5px;
-    border-bottom: solid 1px #ECECEC;
-
+    width: 32.3%;
+    border-radius: 5px;
+    margin-right: 2.5px;
   }
-  .imgdiv{
-    border: 1px black;
-    zoom: 1;
-    width: 100%;
 
-  }
-  .style1_title{
-    float: right;
+  .video_img {
     margin-top: 14px;
-    margin-left: 21px;
+    height: 169px;
+    float: left;
+    border-radius: 5px;
+    width: 100%;
+    margin-right: 5px;
   }
-  .style1_img{
+
+  .type2_title {
+    margin: 14px 18px;
+  }
+
+  .type2_div {
+    margin: 14px 18px;
+    height: 271px;
+    border-bottom: solid 1px #ECECEC;
+  }
+
+  .style2_img_div {
+  }
+
+  .style1_title {
+    margin-top: 28px;
+    margin-left: 149px;
+    margin-bottom: 14px;
+  }
+
+  .style1_date_div {
+    margin-left: 149px;
+    margin-top: 10px;
+
+  }
+
+  .style1_img {
     margin-top: 14px;
     height: 110px;
     width: 110px;
+    border-radius: 5px;
     float: left;
+  }
+
+  .style2_title {
+    margin-left: 0px;
+  }
+
+  .style1_img_div {
+    float: left;
+    margin-top: 0px;
+
+  }
+
+  .style2_date_div {
+    margin-left: 0px;
   }
 
 </style>
